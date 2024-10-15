0027fbf44201ea966535f4cbff696fa477220fb3
Abhay31 <abhaysharma0983@gmail.com>
2024-10-16 00:15:55 +0530

    new_chngs_Abhay_16-10-24

--- src/App.js
+++ src/App.js
@@ -10,16 +10,23 @@ import './custom.css'
 import './responsive-bkp.css'
 import './owl.css'
 import './nice-select.css'
+import Footer from './components/Footer/Footer';
+import WhatsAppChat from './components/WhatsAppChat/WhatsAppChat';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Contact from './components/Contact/Contact';
 
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/about" element={<About />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/header" element={<Header />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <ScrollToTop />
+      <WhatsAppChat />
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Footer.css';  // Assuming custom styles for padding/margin adjustments
+
+const Footer = () => {
+    return (
+        <footer className="main-footer fixed-bottom">
+            <div className="footer-bottom centred">
+                <div className="auto-container">
+                    <div className="bottom-inner">
+                        <div className="copyright">
+                            <p>
+                                Copyright &copy; 2024 <a href="#.">KBMC</a>. All rights reserved. 
+                                <span> | </span> 
+                                <a href="#.">Privacy Policy</a> 
+                                <span> | </span> 
+                                <a href="#.">Terms &amp; Conditions</a> 
+                                <span> | </span> 
+                                <a href="/contact">Contact Us</a> 
+                                <span> | </span> 
+                                Total Visitors: <a href="#.">135019</a>.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
