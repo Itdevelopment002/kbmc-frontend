4ee6bf00f06f2089a5b114c77a675ccd78a1a8b2
Abhay31 <abhaysharma0983@gmail.com>
2024-10-17 12:01:16 +0530

    new_chngs_Abhay_17-10-24

---
+++ src/components/Amrut/Amrut.js
@@ -0,0 +1,176 @@
+import React from "react";
+
+const Amrut = () => {
+  return (
+    <div>
+      <section class="page-title ">
+        <div
+          class="bg-layer"
+          style={{
+            backgroundImage: "url(assets/images/banner/inner-banner.jpg)",
+          }}
+        ></div>
+        <div class="line-box">
+          <div class="line-1"></div>
+          <div class="line-2"></div>
+        </div>
+        <div class="auto-container">
+          <div class="content-box">
+            <h1>Amrut</h1>
+            <ul class="bread-crumb clearfix">
+              <li>
+                <a href="#.">Schemes</a>
+              </li>
+              <li>
+                <span>Amrut</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <br />
+      <br />
+
+      <section class="service-style-four">
+        <div class="auto-container">
+          <h4>Ulb Code- 802792</h4>
+          <h4 class="pb-3 pt-2 color_blue">
+            Atal Mission for Rejuvenation and Urban Transformation (Amrit)
+            Mission (AMRUT)
+          </h4>
+          <p class="text-black text-justify">
+            Letter C 14012/95/2015-69-A (Eka) dated 31/07/2015 regarding the
+            implementation of Atal Mission for Dejuvenation and Urban
+            Transformation (Amrit) scheme to improve the standard of living of
+            the people and create clean, sustainable and environmentally
+            friendly cities through the Central Government. Guidelines have been
+            published vide 25/08/2015. Under the scheme, the components of water
+            supply, sewage disposal, sewage treatment, rain-water channels,
+            urban transport, green areas and gardens as well as reform
+            management (reforms) and capacity building are included. Including
+            Government Circular No. AMRUT-2014/Q.No. 202/Navi-33 Urban
+            Development Department, Government of Maharashtra dated 10/08/2015
+            and the Municipal Council General Assembly Resolution No. A
+            resolution has been passed by one vote regarding participation in
+            the said scheme under 168.
+          </p>
+          <div class="table-responsive mt-3">
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <td rowspan="3">2</td>
+                  <th rowspan="3">
+                    Objectives/achievements for the financial year 2023-24
+                  </th>
+                  <th colspan="1">Sr. No.</th>
+                  <th>Matter</th>
+                  <th colspan="3">Physical</th>
+                  <th colspan="4">Financial</th>
+                  <th rowspan="3">Shera</th>
+                </tr>
+                <tr>
+                  <td rowspan="2">1</td>
+                  <td rowspan="2">Sewerage Scheme</td>
+                  <td colspan="1">Purpose</td>
+                  <td>Achieved</td>
+                  <td>Percentage</td>
+                  <td>Expenditure</td>
+                  <td>Funds available</td>
+                  <td>Cost</td>
+                  <td>Percentage</td>
+                </tr>
+                <tr>
+                  <td>100%</td>
+                  <td>75%</td>
+                  <td>75%</td>
+                  <td>47.15 Cr.</td>
+                  <td>47.15 Cr.</td>
+                  <td>39.63 Cr.</td>
+                  <td>84%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="table-responsive mt-3">
+            <table class="table table-bordered">
+              <tbody>
+                <tr>
+                  <td rowspan="3">3</td>
+                  <th rowspan="3">
+                    Objectives/achievements for the financial year 2023-24
+                  </th>
+                  <th colspan="1">Sr. No.</th>
+                  <th>Matter</th>
+                  <th colspan="3">Physical</th>
+                  <th colspan="4">Financial</th>
+                  <th rowspan="3">Shera</th>
+                </tr>
+                <tr>
+                  <td rowspan="2">1</td>
+                  <td rowspan="2">Sewerage Scheme</td>
+                  <td colspan="1">Purpose</td>
+                  <td>Achieved</td>
+                  <td>Percentage</td>
+                  <td>Expenditure</td>
+                  <td>Funds available</td>
+                  <td>Cost</td>
+                  <td>Percentage</td>
+                </tr>
+                <tr>
+                  <td>100%</td>
+                  <td>75%</td>
+                  <td>75%</td>
+                  <td>47.15 Cr.</td>
+                  <td>47.15 Cr.</td>
+                  <td>39.63 Cr.</td>
+                  <td>84%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <p class="text-black text-justify">
+            Kulgaon Badlapur Municipal Council has implemented Phase-1 Project
+            of Sewage Disposal Scheme under Jawaharlal Nehru National Urban
+            Reconstruction Mission, taking into consideration the instructions
+            of Hon'ble National Green Judge, Pune to maintain the balance of
+            river and environment by implementing sewage disposal scheme. In
+            these projects, 22 D.L. Ltd. 01 S.T.P., 03 Pumping Stations per day
+            capacity and 80.93 km. The work of long sewer network has been
+            completed and the said project dt. It has been operational since 31
+            May 2016. Hon. May in the Supreme Court. According to the petition
+            filed by Vanshakti, for the remaining and new areas of Kulgaon
+            Badlapur Municipal Council, 6 pumping stations and new 2 STPs have
+            been constructed there by blocking the main drains leading to Ulhas
+            river in the city. The proposal of Rs.31.44 Crores for making work
+            under Amrut Yojana Government Decision No. Amrit-
+            2017/Pr.No.566/Navi-33 dated 07/12/2017 has been approved. As no
+            contractor's tender has been received despite calling for open
+            e-tenders for the said work 4 times, the work of Mal: ​​Drainage
+            Scheme (Phase-2) approved by Kulgaon- Badlapur Municipal Council
+            under Amrit Yojana has not been classified as an implemented system
+            by Maharashtra Life Authority. Pa. Special meeting dt. 27/06/2018
+            Resolution no. 25 has been approved and dt. It has been approved by
+            the Government on 28/06/2018. According to the approval of the
+            government, the municipal council has classified the said scheme as
+            a mechanism implemented by the Maharashtra Life Authority and the
+            municipal council has so far received Rs.23.57 crores from the
+            central government and Rs.11.78 crores from the state government and
+            N.P. Share Rs.11.78 Crores Total Rs. A grant amounting to 47.13
+            crores has been classified. As no contractor's tender has been
+            received despite calling for e-tenders for the said work 4 times,
+            the work of 'Salt Disposal Scheme (Phase-2) approved by Kulgaon
+            Badlapur Municipal Council under Amrut Yojana in Municipal Council
+            Special Meeting dated 27/06/2018 has been entrusted to Maharashtra
+            Life Authority as the implementing mechanism. Classified and the
+            Maharashtra Life Authority called for e-tenders and the contractor
+            May. To Dream Constructions, Nandurbar. The work order has been
+            given on 20/09/2019 and the work of the scheme is in progress and it
+            is planned to implement the scheme by the end of 2024.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Amrut;
---
+++ src/components/GenAdminDept/GenAdminDept.js
@@ -0,0 +1,172 @@
+import React from "react";
+
+const GenAdminDept = () => {
+  return (
+    <div>
+      <section class="page-title ">
+        <div
+          class="bg-layer"
+          style={{
+            backgroundImage: "url(assets/images/banner/inner-banner.jpg)",
+          }}
+        ></div>
+        <div class="line-box">
+          <div class="line-1"></div>
+          <div class="line-2"></div>
+        </div>
+        <div class="auto-container">
+          <div class="content-box">
+            <h1>General Admin Department</h1>
+            <ul class="bread-crumb clearfix">
+              <li>
+                <a href="departments.php">Departments</a>
+              </li>
+              <li>
+                <span>General Admin Department</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <br />
+      <section class="departments-style-two alternat-2">
+        <div class="auto-container">
+          <div class="row clearfix">
+            <div class="col-lg-9 col-md-12 col-sm-12 content-side">
+              <div class="department-details-content">
+                <div class="content-one dept_leyer_1">
+                  <div class="title-box">
+                    <h3>General Admin Department</h3>
+                    <hr />
+                  </div>
+                  <ul>
+                    <li>
+                      <a href="/dept-layer-2">
+                        General Meeting and Standing Committee Meeting
+                        Resolutions
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-12 col-sm-12 sidebar-side">
+              <div class="department-sidebar">
+                <div class="category-widget">
+                  <div class="widget-content">
+                    <ul class="category-list clearfix">
+                      <li>
+                        {" "}
+                        <a href="#.">General Admin Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="audit-dept.php">Audit Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="tax-department.php">Tax Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="account-dept.php">Account Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="town-planning.php">Town Planning</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="electrical-dept.php">Electrical Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="public-work-dept.php">
+                          Public Work Department (PWD)
+                        </a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="income-dept.php">Milkat (Income)</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="egovernance-dept.php">
+                          E-Governance Department
+                        </a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="health-dept.php">Health Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="wcd.php">WCD (Women and Child Development)</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="advertisement-dept.php">
+                          Advertisement Department
+                        </a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="education-dept.php">Education Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="#.">Security Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="vehicle-dept.php">Vehicle Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="nulm.php">NULM Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="hospital-dept.php">Hospital Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="fire-dept.php">Fire Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="legal-dept.php">Legal Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="disability-welfare-dept.php">
+                          Disability Welfare
+                        </a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="#.">Store & Records Department</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="#.">Marriage Registration</a>
+                      </li>
+                      <li>
+                        {" "}
+                        <a href="birth-and-death-dept.php">
+                          Birth & Death Department
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default GenAdminDept;
